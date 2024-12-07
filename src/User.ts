02772f65bbba55214e63b60c9e9b01145c084291
Maksym Pryhoda <maxum23025@gmail.com>
2024-12-07 21:05:15 +0200

    add get user by id schema

--- src/User.ts
+++ src/User.ts
@@ -4,3 +4,7 @@ export const UserRecordSchema = z.object({
   firstName: z.string().min(1).optional(),
   lastName: z.string().min(1).optional(),
 });
+
+export const GetUserByIdSchema = z.object({
+  id: z.coerce.number()
+})
